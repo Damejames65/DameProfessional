9091686967aeb3487ece3277743f468bfdd93e23
Damejames65 <damienfvj@gmail.com>
2021-12-16 14:00:11 -0500

    mixed

--- moon.py
+++ moon.py
@@ -1,37 +1,42 @@
-myString= "This is a string"
-print(myString)
-print(type(myString))
+# myString= "This is a string"
+# print(myString)
+# print(type(myString))
 
-print(myString + " is the data type " + str(type(myString)))
+# print(myString + " is the data type " + str(type(myString)))
 
-# Concatonating
+# # Concatonating
 
-FirstString = "Water"
-secondstring = 'Fall'
-thirdstring = FirstString +  secondstring
-print(thirdstring)
+# FirstString = "Water"
+# secondstring = 'Fall'
+# thirdstring = FirstString +  secondstring
+# print(thirdstring)
 
-# name = input("What is your name?")
-# color = input("What is your favorite color?")
-# animal = input("What is your favorite animal?")
-# print("{}, you like a {} {}!" .format(name, color, animal))
+# # name = input("What is your name?")
+# # color = input("What is your favorite color?")
+# # animal = input("What is your favorite animal?")
+# # print("{}, you like a {} {}!" .format(name, color, animal))
 
-myFruitList = ["apple", "pineapple", "banana"]
+# myFruitList = ["apple", "pineapple", "banana"]
 
-myFruitList[1] = "orange"
-print(myFruitList)
+# myFruitList[1] = "orange"
+# print(myFruitList)
 
-favoriteRestaraunts = ("Bojangles", "Mcdonalds", "Popeyes")
-print(favoriteRestaraunts[0])
-print(favoriteRestaraunts[2])
+# favoriteRestaraunts = ("Bojangles", "Mcdonalds", "Popeyes")
+# print(favoriteRestaraunts[0])
+# print(favoriteRestaraunts[2])
 
-print(f"My favorite restaraunt is {favoriteRestaraunts[0]}")
+# print(f"My favorite restaraunt is {favoriteRestaraunts[0]}")
 
-myFavoriteFruitDictionary = {
-    "Akua" : "apple",
-    "Saanvi" : "banana",
-    "Paulo" : "pineapple"
-}
-print(myFavoriteFruitDictionary)
-print(type(myFavoriteFruitDictionary))
-print(myFavoriteFruitDictionary["Paulo"])
+# myFavoriteFruitDictionary = {
+#     "Akua" : "apple",
+#     "Saanvi" : "banana",
+#     "Paulo" : "pineapple"
+# }
+# print(myFavoriteFruitDictionary)
+# print(type(myFavoriteFruitDictionary))
+# print(myFavoriteFruitDictionary["Paulo"])
+
+MixedList = [23, "Jordan", True, False, 2021, "Damien"]
+print(MixedList)
+for item in MixedList:
+    print"{} is of the data type {}" .format(item,type(item)))
