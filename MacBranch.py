8058f4f617b7d0ec7a50f52e0becfb5ca6178864
Damejames65 <damienfvj@gmail.com>
2021-12-17 15:02:01 -0500

    loops

--- MacBranch.py
+++ MacBranch.py
@@ -1,3 +1,18 @@
-# Commit
+# Printing to the console
 bestFood = "Pizza"
 print(bestFood)
+print(type("Hello sirt"))
+# Creating a concatonated String
+
+sent= "Hey yo,"
+weather= "What is the weather like today? "
+name= input("What is your name good sir?")
+
+combineSTR= Sent + " " + weather + " " + name
+
+print(combineSTR) 
+
+# Loop
+print("Count to 10!")
+for x in range(0,100):
+    print(x)
